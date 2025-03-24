1dc37b164e9ac4a9ec9914311c9241eef34cc957
hajmohamad <mohamadrahi98@gmail.com>
2025-03-25 00:41:20 +0330

    remove if

--- Program.cs
+++ Program.cs
@@ -104,11 +104,10 @@ builder.Services.AddScoped<IAdminRepository, AdminRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
