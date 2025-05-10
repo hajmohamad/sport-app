b83b416a4555691ad1d96d7fcf8ef2ff787c1d2e
hajmohamad <mohamadrahi98@gmail.com>
2025-05-10 23:57:05 +0330

    add migration

---
+++ Migrations/20250510200027_add_authority.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sport_app_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_authority : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TransactionId",
+                table: "Payments",
+                newName: "Authority");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Authority",
+                table: "Payments",
+                newName: "TransactionId");
+        }
+    }
+}
