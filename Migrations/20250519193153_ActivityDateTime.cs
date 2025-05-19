24ab9da26862093ed6912fb0b6c7cdb725149ebb
hajmohamad <mohamadrahi98@gmail.com>
2025-05-20 01:31:06 +0330

    migrations add

---
+++ Migrations/20250519193153_ActivityDateTime.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sport_app_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActivityDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Activities",
+                newName: "Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Activities",
+                newName: "DateTime");
+        }
+    }
+}
