49521714ed4c9ef5f2d688e1b09a83fcb2bca91e
hajmohamad <mohamadrahi98@gmail.com>
2025-07-12 21:53:12 +0330

    add AppFee migrations

---
+++ Migrations/20250712182230_addAppFee.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sport_app_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAppFee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AppFee",
+                table: "Payments",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppFee",
+                table: "Payments");
+        }
+    }
+}
