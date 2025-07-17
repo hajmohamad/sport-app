6a512ff27ca690a9b133fb74c2f1f8335e675420
hajmohamad <mohamadrahi98@gmail.com>
2025-07-18 00:07:14 +0330

    comment serilog

--- Handler/GlobalExceptionHandler.cs
+++ Handler/GlobalExceptionHandler.cs
@@ -1,49 +1,49 @@
-using Microsoft.AspNetCore.Diagnostics;
-using System.Diagnostics;
-using sport_app_backend.Interface;
-
-namespace sport_app_backend.Handler;
-
-public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger, ISmsService smsService)
-    : IExceptionHandler
-{
-    public async ValueTask<bool> TryHandleAsync(
-        HttpContext httpContext,
-        Exception exception,
-        CancellationToken cancellationToken)
-    {
-        var traceId = Activity.Current?.Id ?? httpContext.TraceIdentifier;
-
-        
-        logger.LogError(
-            exception,
-            "on handel error TraceId: {TraceId}, Path: {Path}",
-            traceId,
-            httpContext.Request.Path
-        );
-
-
-        try
-        {
-            await smsService.SendErrorSms(httpContext.Request.Path);
-        }
-        catch (Exception smsEx)
-        {
-            logger.LogError(smsEx, "خطا در هنگام ارسال پیامک اضطراری.");
-        }
-
-
-        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        await httpContext.Response.WriteAsJsonAsync(new
-        {
-            Title = "خطای داخلی سرور",
-            Status = StatusCodes.Status500InternalServerError,
-            Detail = "یک مشکل غیرمنتظره در سرور رخ داده است. لطفاً بعداً تلاش کنید.",
-            TraceId = traceId
-            
-        }, cancellationToken);
-
-        return true;
-    }
-}
-    
+// using Microsoft.AspNetCore.Diagnostics;
+// using System.Diagnostics;
+// using sport_app_backend.Interface;
+//
+// namespace sport_app_backend.Handler;
+//
+// public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger, ISmsService smsService)
+//     : IExceptionHandler
+// {
+//     public async ValueTask<bool> TryHandleAsync(
+//         HttpContext httpContext,
+//         Exception exception,
+//         CancellationToken cancellationToken)
+//     {
+//         var traceId = Activity.Current?.Id ?? httpContext.TraceIdentifier;
+//
+//         
+//         logger.LogError(
+//             exception,
+//             "on handel error TraceId: {TraceId}, Path: {Path}",
+//             traceId,
+//             httpContext.Request.Path
+//         );
+//
+//
+//         try
+//         {
+//             await smsService.SendErrorSms(httpContext.Request.Path);
+//         }
+//         catch (Exception smsEx)
+//         {
+//             logger.LogError(smsEx, "خطا در هنگام ارسال پیامک اضطراری.");
+//         }
+//
+//
+//         httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+//         await httpContext.Response.WriteAsJsonAsync(new
+//         {
+//             Title = "خطای داخلی سرور",
+//             Status = StatusCodes.Status500InternalServerError,
+//             Detail = "یک مشکل غیرمنتظره در سرور رخ داده است. لطفاً بعداً تلاش کنید.",
+//             TraceId = traceId
+//             
+//         }, cancellationToken);
+//
+//         return true;
+//     }
+// }
+//     
