36b931e1e37cee0b06fd675fa39a4a3a43b60bf1
mohaamd <mohamadrahi98@gmail.com>
2025-11-29 10:55:37 +0330

    edit cors policy

--- Program.cs
+++ Program.cs
@@ -36,11 +36,20 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontendLocalhost", policy =>
     {
-        policy.WithOrigins("http://localhost:21345","https://charsetpwa.liara.run","https://charset-pwa-staging.liara.run/","https://app.chaarset.ir","https://chaarset.ir", "https://charset-i-os-pwa.vercel.app","https://charset-pwa.pages.dev") 
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+        policy.WithOrigins(
+                "http://localhost:21345",
+                "https://charsetpwa.liara.run",
+                "https://charset-pwa-staging.liara.run",
+                "https://app.chaarset.ir",
+                "https://chaarset.ir",
+                "https://charset-i-os-pwa.vercel.app",
+                "https://charset-pwa.pages.dev"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
     });
 });
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
