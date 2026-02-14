b0a75c8f962e3e99738ec888b2b5e88cdc76443d
mohaamd <mohamadrahi98@gmail.com>
2026-02-15 01:27:31 +0330

    add migrations for ProgramRenewalReminderService

---
+++ Migrations/20260214205204_AddRenewalReminderSentToWorkoutProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sport_app_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRenewalReminderSentToWorkoutProgram : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "RenewalReminderSent",
+                table: "WorkoutPrograms",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RenewalReminderSent",
+                table: "WorkoutPrograms");
+        }
+    }
+}
