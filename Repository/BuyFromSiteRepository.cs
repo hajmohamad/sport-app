25b2eac3fad35e45d343ab673b56c0f8ef68a35c
mohamad <mohamadrahi98@gmail.com>
2026-05-15 22:17:53 +0330

    fix bugs

--- Repository/BuyFromSiteRepository.cs
+++ Repository/BuyFromSiteRepository.cs
@@ -1120,7 +1120,7 @@ public class BuyFromSiteRepository(
             return new ApiResponse { Action = false, Message = "کد تاریخش گذشته" };
         }
 
-        if (discountCode.CoachServicesId is not null && !discountCode.CoachServicesId.Contains(coachServiceId))
+        if (discountCode.CoachServicesId is not null && (!discountCode.CoachServicesId.Contains(coachServiceId)&&discountCode.CoachServicesId.Count!=0))
         {
             return new ApiResponse { Action = false, Message =  "کد تخفیف برای این سرویس قابل استفاده نیست" };
         }
