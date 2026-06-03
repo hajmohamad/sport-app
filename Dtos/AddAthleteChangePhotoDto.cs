8708c5a761b69b38137c89546adeb422c1ccf99a
mohamad <mohamadrahi98@gmail.com>
2026-06-03 13:49:52 +0330

    edit dto

--- Dtos/AddAthleteChangePhotoDto.cs
+++ Dtos/AddAthleteChangePhotoDto.cs
@@ -13,6 +13,6 @@ public class EditAthleteChangePhotoDto
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public IFormFile PhotoUrl { get; set; } = default!;
+    public IFormFile? PhotoUrl { get; set; } = default!;
 
 }
